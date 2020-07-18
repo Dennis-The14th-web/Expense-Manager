affd105117adbeba0dbdce943231c117868a24a8
the 14th web <dennis.itua1@gmail.com>
2020-07-18 17:51:04 -0400

    updating models and server

--- server/server.js
+++ server/server.js
@@ -2,12 +2,19 @@
 var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
-
+var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+mongoose.connect('mongodb://heroku_nl1zntbx:o5kibl4l8f4nj371976kaqqaci@ds261238.mlab.com:61238/heroku_nl1zntbx');
 
 app.use('/', router);
 
